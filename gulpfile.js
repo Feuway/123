091a28bbf0aab402d0ae366d437c2b6eeffea150
Denis <feuway@gmail.com>
2016-11-01 16:57:49 +0300

    Окончена верстка

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,28 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require("gulp-sass");
+var sass = require('gulp-sass');
+var server = require('browser-sync');
 
-gulp.task("copy", function() {
-    return gulp.src([
-        "src/fonts/**/*.{woff,woff2}",
-        "src/img/**",
-        "src/js/**",
-        "src/*.html"
-    ])
-        .pipe(gulp.dest("dest"));
-});
-   
 gulp.task('style', function() {
-    gulp.src('src/sass/style.scss')
+    gulp.src('sass/style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('src/css'))
-        .pipe(gulp.dest('dest/css'));
+        .pipe(gulp.dest('css'))
+        .pipe(server.reload({
+            stream: true
+        }))
 });
+
+gulp.task('server', ['style'], function() {
+    server.init({
+        server: ".",
+        notify: false,
+        open: true,
+        ui: false
+    });
+    gulp.watch('sass/style.scss', ['style'], server.reload);
+    gulp.watch('*.html', server.reload);
+    gulp.watch('js/*.js', server.reload);
+});
+
+
